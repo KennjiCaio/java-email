81245c47cbffb27069cfd6b6f06123ff2a058d6d
Kenji <caio_yariwake@hotmail.com>
2019-10-09 12:36:16 -0300

    Add comments

--- src/mailutil/JavaMailUtil.java
+++ src/mailutil/JavaMailUtil.java
@@ -17,11 +17,12 @@ public class JavaMailUtil {
 	public static void sendMail(Email email) throws MessagingException {
 		System.out.println("Preparing to send email");
 		
+		// propriedades de configuracao de um email
 		Properties properties = new Properties();
 		properties.put("mail.smtp.auth", "true");
 		properties.put("mail.smtp.starttls.enable", "true");
-		properties.put("mail.smtp.host", "smtp.gmail.com");
-		properties.put("mail.smtp.port", "587");
+		properties.put("mail.smtp.host", "smtp.gmail.com"); //smtp do Gmail
+		properties.put("mail.smtp.port", "587"); //porta
 		
 		Session session = Session.getInstance(properties, new Authenticator() {
 			@Override
@@ -39,10 +40,12 @@ public class JavaMailUtil {
 	private static Message prepareMassage(Session session, Email email) {
 		try {
 			Message message = new MimeMessage(session);
+			// Configurações do corpo do email
 			message.setFrom(new InternetAddress(email.getMyEmail()));
 			message.setRecipient(Message.RecipientType.TO, new InternetAddress(email.getSendToEmail()));
 			message.setSubject(email.getSubject());
 			message.setText(email.getText());
+			
 			return message;
 		}
 		catch(Exception ex) {
